236535568a4d698a0b79de77c2a1124b115bcbb9
Alex Li <xiangfly1104@gmail.com>
2015-10-20 20:47:02 -0400

    Totally redesign MapViewContrller

--- MyAssistant/MyAssistant/MapViewController.h
+++ MyAssistant/MyAssistant/MapViewController.h
@@ -2,32 +2,12 @@
 //  MapViewController.h
 //  MyAssistant
 //
-//  Created by Jiaxiang Li on 15/8/23.
-//  Copyright (c) 2015年 Jiaxiang Li. All rights reserved.
+//  Created by Jiaxiang Li on 10/20/15.
+//  Copyright © 2015 Jiaxiang Li. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import <CoreLocation/CoreLocation.h>
 
-@interface MapViewController : UIViewController<UITextFieldDelegate>
-
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *slideMenuBar;
-
-@property (weak, nonatomic) IBOutlet UITextField *startStreet;
-
-
-@property (weak, nonatomic) IBOutlet UITextField *startCity;
-
-@property (weak, nonatomic) IBOutlet UITextField *startState;
-@property (weak, nonatomic) IBOutlet UITextField *startZip;
-
-@property (weak, nonatomic) IBOutlet UITextField *endStreet;
-
-@property (weak, nonatomic) IBOutlet UITextField *endCity;
-
-
-@property (weak, nonatomic) IBOutlet UITextField *endState;
-
-@property (weak, nonatomic) IBOutlet UITextField *endZip;
+@interface MapViewController : UIViewController
 
 @end
--- MyAssistant/MyAssistant/MapViewController.m
+++ MyAssistant/MyAssistant/MapViewController.m
@@ -2,37 +2,28 @@
 //  MapViewController.m
 //  MyAssistant
 //
-//  Created by Jiaxiang Li on 15/8/23.
-//  Copyright (c) 2015年 Jiaxiang Li. All rights reserved.
+//  Created by Jiaxiang Li on 10/20/15.
+//  Copyright © 2015 Jiaxiang Li. All rights reserved.
 //
 
 #import "MapViewController.h"
 #import "SWRevealViewController.h"
-#import "DirectionViewController.h"
-@import AddressBook;
-@interface MapViewController ()<CLLocationManagerDelegate,UITextFieldDelegate> {
-    NSString *sStreetAddress;
-    NSString *sCityName;
-    NSString *sStateName;
-    NSString *sZip;
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface MapViewController ()<UISearchBarDelegate,CLLocationManagerDelegate,MKMapViewDelegate,UITableViewDataSource,UITableViewDelegate> {
     
-    NSString *dStreetAddress;
-    NSString *dCityName;
-    NSString *dStateName;
-    NSString *dZip;
+    NSString *searchBarInput;
+    NSMutableArray *annotations;
+    NSMutableArray *resultPlacemark;
     
-    CLLocationCoordinate2D __block startCoordinate;
-    
-    CLLocationCoordinate2D __block endCoordinate;
-    
-    
-    MKMapItem *startMapItem;
-    
-    MKMapItem *endMapItem;
 }
-
-@property (nonatomic,strong) CLLocationManager *locationManager;
-
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *slideMenuButton;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong,nonatomic) CLLocationManager *locationManager;
+@property (strong,nonatomic) CLLocation *currentLocation;
 
 @end
 
@@ -42,29 +33,27 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    
     SWRevealViewController *revealViewController = self.revealViewController;
     
     if (revealViewController) {
-        [self.slideMenuBar setTarget:self.revealViewController];
+        [self.slideMenuButton setTarget:self.revealViewController];
         
-        [self.slideMenuBar setAction:@selector(revealToggle:)];
+        [self.slideMenuButton setAction:@selector(revealToggle:)];
         
         [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
     }
     
-
+    annotations = [[NSMutableArray alloc] init];
+    resultPlacemark = [[NSMutableArray alloc] init];
     
-    self.endCity.delegate = self;
-    self.endState.delegate = self;
-    self.endStreet.delegate = self;
-    self.endZip.delegate = self;
+    self.searchBar.delegate = self;
+    self.mapView.delegate = self;
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
     
-    UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hidenKeyboard)];
+    [self configueCLLocationManager];
     
-    gesture.numberOfTapsRequired = 1;
-    [self.view addGestureRecognizer:gesture];
-
+    self.mapView.showsUserLocation = YES;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -73,277 +62,172 @@
 }
 
 
+
+
+#pragma mark - CLLocationManager Configuation & CLLocationManager Delegate
+
+-(void) configueCLLocationManager {
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    self.locationManager.distanceFilter = 50;
+    self.locationManager.delegate = self;
+    [self.locationManager requestWhenInUseAuthorization];
+    
+    [self.locationManager startUpdatingLocation];
+}
+
+
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    self.currentLocation = [locations lastObject];
+    
+    
+    [self.mapView setRegion:MKCoordinateRegionMakeWithDistance(self.currentLocation.coordinate, 5000, 5000)];
+    
+    
+    
+}
+
+
+#pragma mark - SearchBar Delegate
+
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    searchBarInput = self.searchBar.text;
+    [self.searchBar resignFirstResponder];
+    if (annotations.count > 0) {
+        [self.mapView removeAnnotations: annotations];
+        [annotations removeAllObjects];
+        [resultPlacemark removeAllObjects];
+    }
+    [self performSearchWithInput:searchBarInput];
+    
+}
+
+-(void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    
+}
+
+
+#pragma mark - Perform Search
+
+-(void) performSearchWithInput:(NSString *) inputContent {
+    
+    
+    if (inputContent.length > 0) {
+        NSLog(@"%.6f",self.currentLocation.coordinate.latitude);
+        
+        MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(self.currentLocation.coordinate, 5000, 5000);
+        MKLocalSearchRequest *localRequest = [[MKLocalSearchRequest alloc] init];
+        localRequest.naturalLanguageQuery = inputContent;
+        localRequest.region = region;
+        
+        MKLocalSearch *search = [[MKLocalSearch alloc] initWithRequest:localRequest];
+
+        
+        [search startWithCompletionHandler:^(MKLocalSearchResponse * _Nullable response, NSError * _Nullable error) {
+            if (error) {
+                NSLog(@"Search Request Error:%@",error.localizedDescription);
+            }
+            
+            if (response.mapItems.count == 0) {
+                //Do Something to implement this situation
+                NSLog(@"No Response!");
+            }
+            
+            for (MKMapItem *item in response.mapItems) {
+                MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+                MKPlacemark *placemark = item.placemark;
+                annotation.subtitle = [NSString stringWithFormat:@"%@ %@,%@,%@,%@",placemark.subThoroughfare,placemark.thoroughfare,placemark.locality,placemark.administrativeArea,placemark.postalCode];
+                annotation.title = placemark.name;
+                annotation.coordinate = placemark.coordinate;
+                
+                [annotations addObject:annotation];
+                [resultPlacemark addObject:placemark];
+                [self.mapView addAnnotation:annotation];
+                
+            }
+            
+        }];
+        
+    }
+    
+}
+
+
+#pragma mark - MapView Delegate
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    
+    
+    if ([annotation isKindOfClass:[MKUserLocation class]]) {
+        
+        return  nil;
+    }
+    
+    MKPinAnnotationView *pinAnnotationView = nil;
+    
+    if ([annotation isKindOfClass:[MKPointAnnotation class]]) {
+        static NSString *identifier = @"annotationView";
+        pinAnnotationView = (MKPinAnnotationView *) [mapView dequeueReusableAnnotationViewWithIdentifier:identifier];
+        
+        if (!pinAnnotationView) {
+            pinAnnotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:identifier];
+        }
+        
+        pinAnnotationView.canShowCallout = YES;
+        pinAnnotationView.animatesDrop = YES;
+        pinAnnotationView.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    }
+    
+    [self.tableView reloadData];
+    
+    return pinAnnotationView;
+    
+}
+
+-(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
+    MKAnnotationView *currentAnnotation = [mapView viewForAnnotation:userLocation];
+    currentAnnotation.canShowCallout = YES;
+
+}
+
+#pragma mark - UITableViewDataSource Delegate & UITableView Delegate
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *identifier = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    MKPlacemark *placemark = resultPlacemark[indexPath.row];
+    cell.textLabel.text = placemark.name;
+    
+    return cell;
+    
+}
+
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return annotations.count;
+}
+
+
+
+
+/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
-    
-    UINavigationController *navigationController = [segue destinationViewController];
-    
-    
-    DirectionViewController *dirViewController = (DirectionViewController *) [[navigationController viewControllers] firstObject];
-    
-    
-    dirViewController.startItem = startMapItem;
-    
-    dirViewController.endItem = endMapItem;
-    
-    dirViewController.startCoordinate = startCoordinate;
-    
-    NSLog(@"==============%.6f",startMapItem.placemark.coordinate.latitude);
-    
 }
-
-
-#pragma mark - Action
-
-
-- (IBAction)getCurrentLocationAction:(id)sender {
-    
-    [self getMyLocation];
-    
-}
-
-- (IBAction)getDirectionAction:(id)sender {
-    
-    dStreetAddress = self.endStreet.text;
-    
-    dCityName = self.endCity.text;
-    
-    dStateName = self.endState.text;
-    
-    dZip = self.endZip.text;
-    
-    
-    sStreetAddress = self.startStreet.text;
-    
-    sCityName = self.startCity.text;
-    
-    sStateName = self.startState.text;
-    
-    sZip = self.startZip.text;
-    
-    [self createMKMapItem];
-    
-    //[self performSegueWithIdentifier:@"goToDirection" sender:self];
-    
-    
-}
-
-
--(void) createMKMapItem {
-    
-    CLGeocoder *startGeocoder = [[CLGeocoder alloc] init];
-    
-    CLGeocoder *endGecoder = [[CLGeocoder alloc] init];
-    
-    NSString *startAddress = [NSString stringWithFormat:@"%@ %@ %@ %@",sStreetAddress,sCityName,sStateName,sZip];
-    
-    NSString *endAddress = [NSString stringWithFormat:@"%@ %@ %@ %@",dStreetAddress,dCityName,dStateName,dZip];
-    DirectionViewController *testViewController = [[DirectionViewController alloc] init];
-    //CLLocation __block *startLocation = [[CLLocation alloc] init];
-    //CLLocation __block *endLocation = [[CLLocation alloc] init];;
-    //CLPlacemark __block *startPlacemark = [[CLPlacemark alloc] init] ;
-    //CLPlacemark __block *endPlacemark = [[CLPlacemark alloc] init];
-    
-    [startGeocoder geocodeAddressString:startAddress completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
-        
-        if (!error) {
-            if (placemarks && placemarks.count > 0) {
-                CLPlacemark *placemark = [placemarks firstObject];
-                //startPlacemark = placemark;
-                CLLocation *startLocation = placemark.location;
-                
-                
-                startCoordinate = startLocation.coordinate;
-                
-                MKPlacemark *startPlacemark1 = [[MKPlacemark alloc] initWithCoordinate:startCoordinate addressDictionary:nil];
-                
-                startMapItem = [[MKMapItem alloc] initWithPlacemark:startPlacemark1];
-                
-                testViewController.startItem = startMapItem;
-                NSLog(@"lat:%.6f,lon:%.6f",startCoordinate.latitude,startCoordinate.longitude);
-            }//end placemark
-        }// end error
-        else {
-            NSLog(@"Eroor4444444:%@",error.localizedDescription);
-        }
-    }];
-    
-    
-    [endGecoder geocodeAddressString:endAddress completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
-        
-        if (!error) {
-            if (placemarks && placemarks.count > 0) {
-                CLPlacemark *placemark = [placemarks firstObject];
-                //endPlacemark = placemark;
-                CLLocation *endLocation = placemark.location;
-                
-                
-                endCoordinate = endLocation.coordinate;
-                
-                 MKPlacemark *endPlacemark1 = [[MKPlacemark alloc] initWithCoordinate:endCoordinate addressDictionary:nil];
-                
-                endMapItem = [[MKMapItem alloc] initWithPlacemark:endPlacemark1];
-                NSLog(@"lat:%.6f,lon:%.6f",endCoordinate.latitude,endCoordinate.longitude);
-                
-                //[self performSegueWithIdentifier:@"goToDirection" sender:self];
-                
-                
-                testViewController.endItem = endMapItem;
-                
-                [self.navigationController pushViewController:testViewController animated:YES];
-                
-            }//end placemark
-        }// end error
-        else {
-            NSLog(@"Error:%@",error.localizedDescription);
-        }
-    }];
-
-    
-    
-//    NSDictionary *startAddressDict = @{
-//                                       (NSString *)kABPersonAddressStreetKey:[NSString stringWithFormat:@"%@ %@",startPlacemark.subThoroughfare,startPlacemark.thoroughfare],
-//                                       (NSString *)kABPersonAddressCityKey:startPlacemark.locality,
-//                                       (NSString *)kABPersonAddressStateKey:startPlacemark.administrativeArea,
-//                                       (NSString *)kABPersonAddressZIPKey:startPlacemark.postalCode
-//                                       };
-//    
-//    
-//    
-//    
-//    NSDictionary *endAddressDict = @{
-//                                     (NSString *)kABPersonAddressStreetKey:[NSString stringWithFormat:@"%@ %@",endPlacemark.subThoroughfare,endPlacemark.thoroughfare],
-//                                     (NSString *)kABPersonAddressCityKey:endPlacemark.locality,
-//                                     (NSString *)kABPersonAddressStateKey:endPlacemark.administrativeArea,
-//                                     (NSString *)kABPersonAddressZIPKey:endPlacemark.postalCode
-//                                     };
-    
-    
-    
-//    MKPlacemark *startPlacemark1 = [[MKPlacemark alloc] initWithCoordinate:startCoordinate addressDictionary:nil];
-//    
-//    NSLog(@"***********************%.6f",startCoordinate.latitude);
-//    
-//    MKPlacemark *endPlacemark1 = [[MKPlacemark alloc] initWithCoordinate:endCoordinate addressDictionary:nil];
-//    
-//    
-//    startMapItem = [[MKMapItem alloc] initWithPlacemark:startPlacemark1];
-//    
-//    endMapItem = [[MKMapItem alloc] initWithPlacemark:endPlacemark1];
-//    
-//    //[self performSegueWithIdentifier:@"goToDirection" sender:self];
-//    
-//    DirectionViewController *testViewController = [[DirectionViewController alloc] init];
-//    testViewController.startItem = startMapItem;
-//    testViewController.endItem = endMapItem;
-//    
-//    NSLog(@"????%.6f",testViewController.startItem.placemark.coordinate.latitude);
-    
-    
-//    [self.navigationController pushViewController:testViewController animated:YES];
-}
-
-
-
-
--(void) getMyLocation {
-    
-    self.locationManager = [[CLLocationManager alloc] init];
-    
-    self.locationManager.delegate = self;
-    
-    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    
-    [self.locationManager requestAlwaysAuthorization];
-    
-    
-    [self.locationManager startUpdatingLocation];
-    
-    
-    
-}
-
-
-
-
--(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
-    
-    CLLocation *currentLocation = [locations firstObject];
-    
-    
-    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
-    
-    
-    [geocoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
-        
-        CLPlacemark *myPlacemark = [placemarks firstObject];
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            
-//            dispatch_once_t myQueue;
-            
-            
-//        dispatch_once(&myQueue, ^{
-                self.startStreet.text = [NSString stringWithFormat:@"%@ %@",myPlacemark.subThoroughfare,myPlacemark.thoroughfare];
-                
-                self.startCity.text = myPlacemark.locality;
-            
-                self.startState.text =myPlacemark.administrativeArea;
-                
-                self.startZip.text = myPlacemark.postalCode;
-//            });
-            
-        });
-    }];
-    
-}
-
-
-
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [self.startCity resignFirstResponder];
-    [self.startStreet resignFirstResponder];
-    [self.startState resignFirstResponder];
-    [self.startZip resignFirstResponder];
-    [self.endCity resignFirstResponder];
-    [self.endStreet resignFirstResponder];
-    [self.endState resignFirstResponder];
-    [self.endZip resignFirstResponder];
-    
-}
-
-#pragma mark - UITextFieldDelegate
--(BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
-    NSTimeInterval animationDuration = 0.30f;
-    [UIView beginAnimations:@"resizeForKeyboard" context:nil];
-    [UIView setAnimationDuration:animationDuration];
-    float width = self.view.frame.size.width;
-    float height = self.view.frame.size.height;
-    CGRect rect = CGRectMake(0.0f, -100, width, height);
-    self.view.frame = rect;
-    [UIView commitAnimations];
-    return YES;
-    
-}
-
--(void) hidenKeyboard {
-    [self resumeView];
-}
-
-
--(void) resumeView {
-    NSTimeInterval animationDuration = 0.30f;
-    [UIView beginAnimations:@"resizeForKeyboard" context:nil];
-    [UIView setAnimationDuration:animationDuration];
-    float width = self.view.frame.size.width;
-    float height = self.view.frame.size.height;
-    CGRect rect = CGRectMake(0.0f,60, width, height);
-    self.view.frame = rect;
-    [UIView commitAnimations];
-}
+*/
 
 @end
